a285e0a0927fe5f61c730af8bfd7112fbf48e01c
Ben Woodward <b@benw.me>
2013-01-15 16:27:31 +0000

    add test for post.tags

--- test/unit/post_test.rb
+++ test/unit/post_test.rb
@@ -1,7 +1,11 @@
-require 'test_helper'
+require_relative '../test_helper'
 
 class PostTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "posts have tags" do
+    p = Post.new
+    p.tag_list = 'banana,orange,blueberry'
+    p.save
+    tags = p.tags.collect { |t| t.name }
+    assert tags.include?('orange'), tags.join(',')
+  end
 end
