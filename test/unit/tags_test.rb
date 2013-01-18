9b7bfc0481837486c2627885ea9b05505d6faeaf
Ben Woodward <b@benw.me>
2013-01-18 16:32:18 +0000

    fix broken test

--- test/unit/tags_test.rb
+++ test/unit/tags_test.rb
@@ -1,7 +1,10 @@
 require_relative '../test_helper'
 
 class TagTest < ActiveSupport::TestCase
+  include ActsAsTaggableOn
+
   test "tags have tag_colour attribute" do
     assert Tag.new.has_attribute?(:tag_colour), Tag.column_names.to_s
   end
+
 end
