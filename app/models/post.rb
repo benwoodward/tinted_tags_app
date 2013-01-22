db27628dec7b45951fc869f0cfd119b0f1c660ec
Ben Woodward <b@benw.me>
2013-01-22 22:51:25 +0000

    add conditional after_save filter to trigger TagTinter

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,13 @@
+require 'tag_tinter'
+
 class Post < ActiveRecord::Base
   attr_accessible :title, :tag_list
   acts_as_taggable
+  after_save :update_tints, if: :tag_list_changed?
+
+  private
+
+  def update_tints
+    TagTinter.new('black', 'white').update_tints
+  end
 end
